88e5ade28fdc550b30f2b76ab8bf1480921f6b99
John Hardin <84035484+BeyondOurMinds@users.noreply.github.com>
2026-02-08 16:47:13 +0000

    initial data analysis and visualization

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,26 @@
 import scanpy as sc
+import pandas as pd
 
 adata = sc.read_h5ad(r"C:\Users\jtspy\OneDrive\Desktop\PersonalProject\NSCLC_Project\NSCLC-scRNA-Analysis\data\raw\figshare\raw_counts.h5ad")
 
+metadata = pd.read_csv(
+    "data/raw/figshare/Metadata.csv",
+    index_col=0
+)
+
 # Basic info
-print(adata)
 
-print(adata.X[:5, :5].toarray())
+adata.obs = metadata
 
-print(adata.obs.head())
+
+adata.write(
+    r"C:\Users\jtspy\OneDrive\Desktop\PersonalProject\NSCLC_Project\NSCLC-scRNA-Analysis\data\raw\figshare\raw_counts_with_metadata.h5ad"
+)
+
+
+sc.pl.violin(
+    adata,
+    keys=["nCount_RNA", "nFeature_RNA", "Percent_mt"],
+    jitter=0.4,
+    multi_panel=True
+)
